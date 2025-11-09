acba3ab5bcbf12b2714838e008e73a70a214463b
Danielle Mayabb <danielle@igalia.com>
2025-11-09 12:10:49 -0800

    Update settings file

--- src/settings.ts
+++ src/settings.ts
@@ -12,14 +12,14 @@ export const profile = {
 export const social = {};
 
 export const template = {
-  website_url: "https://dmtrek14.github.io/remedios-varo", // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+  website_url: "https://dmtrek14.github.io", // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
   menu_left: false,
   transitions: true,
   lightTheme: "stillLife", // Select one of the Daisy UI Themes or create your own
   darkTheme: "varo", // Select one of the Daisy UI Themes or create your own
   excerptLength: 200,
   postPerPage: 5,
-  base: "", // Repository name starting with /
+  base: "/remedios-varo", // Repository name starting with /
 };
 
 export const seo = {
